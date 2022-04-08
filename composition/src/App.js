9985c484173c1f312764fdd0acf71422d9b69a51
Akanksha Patel <a.patel@easternenterprise.com>
2022-04-08 10:36:56 +0530

    composition exampled done

---
+++ composition/src/App.js
@@ -0,0 +1,32 @@
+function Contacts() {
+  return <div className="Contacts">Contacts</div>;
+}
+
+function Chat() {
+  return <div className="Chat">Chat</div>;
+}
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
+      </div>
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
+
+export function App() {
+  return (
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
+  );
+}
---
+++ composition/src/WelcomeDialog.js
@@ -0,0 +1,51 @@
+// Case 1:
+// function FancyBorder(props) {
+//     return (
+//       <div className={'FancyBorder FancyBorder-' + props.color}>
+//         {props.children}
+//       </div>
+//     );
+//   }
+  
+//   export function WelcomeDialog() {
+//     return (
+//       <FancyBorder color="blue">
+//         <h1 className="Dialog-title">
+//           Welcome
+//         </h1>
+//         <p className="Dialog-message">
+//           Thank you for visiting our spacecraft!
+//         </p>
+//       </FancyBorder>
+//     );
+//   }
+
+//Case 2:
+function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+  
+  function Dialog(props) {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          {props.title}
+        </h1>
+        <p className="Dialog-message">
+          {props.message}
+        </p>
+      </FancyBorder>
+    );
+  }
+  
+  export function WelcomeDialog() {
+    return (
+      <Dialog
+        title="Welcome"
+        message="Thank you for visiting our spacecraft!" />
+    );
+  }
